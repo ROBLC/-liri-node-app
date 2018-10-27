95fd0607f948e48b84acfecd5566eafac3384af3
RLC <robz.l.c.77@gmail.com>
2018-10-27 00:41:10 -0500

    Finished Homework!

--- liri.js
+++ liri.js
@@ -1,28 +1,39 @@
 require("dotenv").config();
+var fs = require("fs");
 var Spotify = require('node-spotify-api');
 var moment = require("moment");
 var keys = require("./keys.js");
 var request = require("request");
-var bandsInTownUrl = "https://rest.bandsintown.com/artists/" + userArgs + "/events?app_id=codingbootcamp"
 var userInput = process.argv;
 var command = userInput[2];
 var userArgs = "";
-
-
+var userArgs2 = "";
 for (var i = 3; i < userInput.length; i++) {
     if (i > 3 && i < userInput.length) {
 
         userArgs = userArgs + "+" + userInput[i];
+        userArgs2 = userArgs2 + " " + userInput[i];
     }
     else {
         userArgs += userInput[i];
+        userArgs2 += userInput[i];
     }
 }
-console.log(command);
-console.log(userArgs);
-var bandsInTownUrl = "https://rest.bandsintown.com/artists/" + userArgs + "/events?app_id=codingbootcamp"
-var OMDBUrl = "https://www.omdbapi.com/?t=" + userArgs + "&y=&plot=short&apikey=trilogy";
+console.log("Command: " + command);
+console.log("User search: " + userArgs2);
 function swFun() {
+    log = "Command: " + command + "\n" + "User argument: " + userArgs2 + "\n" + "-------------- \r\n"
+    fs.appendFile("log.txt", log, function (err) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            console.log("Log updated \n");
+        }
+
+    });
+    var bandsInTownUrl = "https://rest.bandsintown.com/artists/" + userArgs + "/events?app_id=codingbootcamp"
+    var OMDBUrl = "https://www.omdbapi.com/?t=" + userArgs + "&y=&plot=short&apikey=trilogy";
     switch (command) {
         case "concert-this":
             request(bandsInTownUrl, function (error, response, data) {
@@ -42,16 +53,19 @@ function swFun() {
         case "spotify-this-song":
             var spotify = new Spotify(keys.spotify);
             if (userArgs) {
-                spotify.request("https://api.spotify.com/v1/search?q=" + userArgs + "&type=track").then(function (data) {
-                    var artists = []
-                    artists += (data.tracks.items[0].artists[0].name);
-                    for (var i = 1; i < data.tracks.items[0].artists.length; i++) {
-                        artists += ", " + (data.tracks.items[0].artists[i].name);
-                    }
+                spotify.request("https://api.spotify.com/v1/search?q=" + userArgs + "&type=track&limit=5").then(function (data) {
+                    for (var s = 0; s < data.tracks.items.length; s++) {
+                        var artists = []
+                        artists += (data.tracks.items[s].artists[0].name);
+                        for (var i = 1; i < data.tracks.items[s].artists.length; i++) {
+                            artists += ", " + (data.tracks.items[s].artists[i].name);
+                        }
 
-                    console.log("Artist: " + artists);
-                    console.log("Song name: " + data.tracks.items[0].name)
-                    console.log("Spotify Link: " + data.tracks.items[0].external_urls.spotify)
+                        console.log("Artist: " + artists);
+                        console.log("Song name: " + data.tracks.items[s].name);
+                        console.log("Spotify Link: " + data.tracks.items[s].external_urls.spotify);
+                        console.log("----------------");
+                    }
                 }).catch(function (err) {
                     console.error('Error occurred: ' + err);
                 });
@@ -73,21 +87,21 @@ function swFun() {
             break;
         case "movie-this":
             request(OMDBUrl, function (error, response, data) {
-                if (!error && response.statusCode === 200)
+                if (!error && response.statusCode === 200) {
                     var movRes = JSON.parse(data);
-                console.log("Title: " + movRes.Title);
-                console.log("Release year: " + movRes.Year);
-                console.log("IMDB Rating: " + movRes.imdbRating);
-                console.log("Rotten Tomatoes Rating: " + movRes.Ratings[1].Value);
-                console.log("Country: " + movRes.Country);
-                console.log("Language(s): " + movRes.Language);
-                console.log("Plot: " + movRes.Plot);
-                console.log("Actors: " + movRes.Actors);
+                    console.log("Title: " + movRes.Title);
+                    console.log("Release year: " + movRes.Year);
+                    console.log("IMDB Rating: " + movRes.imdbRating);
+                    console.log("Rotten Tomatoes Rating: " + movRes.Ratings[1].Value);
+                    console.log("Country: " + movRes.Country);
+                    console.log("Language(s): " + movRes.Language);
+                    console.log("Plot: " + movRes.Plot);
+                    console.log("Actors: " + movRes.Actors);
+                }
+
             })
             break;
         case "do-what-it-says":
-            var fs = require("fs");
-
             fs.readFile("random.txt", "utf8", function (error, data) {
                 if (error) {
                     return console.log(error);
@@ -95,7 +109,15 @@ function swFun() {
                 var dataArr = data.split(",");
                 command = dataArr[0];
                 userArgs = dataArr[1];
-                swFun();
+                userArgs2 = dataArr[1];
+                if (command === "do-what-it-says") {
+                    console.log("If I do that then I'd enter a loop a never ending loop!!!")
+                }
+                else {
+
+                    swFun();
+                }
+
             });
             break;
     }
